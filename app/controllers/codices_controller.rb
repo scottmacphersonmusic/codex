1046122434fdbd6954dc2325a49ea6479d5e2381
Scott Macpherson <scottmacphersonmusic@gmail.com>
2017-09-13 23:26:03 -0700

    update codices success

--- app/controllers/codices_controller.rb
+++ app/controllers/codices_controller.rb
@@ -2,10 +2,17 @@ class CodicesController < ApplicationController
   def create
     @codex = Codex.new(codex_params)
     if @codex.save
-      redirect_to @codex, notice: 'Codex Added'
+      redirect_to @codex, notice: 'Codex Saved'
     else
-      flash[:error] = 'Error Adding Codex'
+      flash[:error] = 'Codex Not Saved'
       render :new
+    end
+  end
+
+  def update
+    @codex = Codex.find(params[:id])
+    if @codex.update_attributes(codex_params)
+      redirect_to @codex, notice: 'Codex Saved'
     end
   end
 
--- spec/controllers/codices_controller_spec.rb
+++ spec/controllers/codices_controller_spec.rb
@@ -1,38 +1,61 @@
 require 'rails_helper'
 
 describe CodicesController do
-  describe 'POST #create' do
+  describe 'POST create' do
     context 'when codices are saved' do
       subject { post :create, codex: attributes_for(:codex) }
 
-      it 'creates codices' do
+      it 'creates a codex' do
         expect{ subject }.to change{ Codex.count }.by(1)
       end
 
-      it 'redirects to :show' do
-        expect(subject).to redirect_to action: :show, id: assigns(:codex).id
-      end
-
-      it 'adds a flash :notice' do
+      it 'assigns flash[:notice]' do
         subject
         expect(flash[:notice]).to be_present
+      end
+
+      it 'redirects to :show' do
+        expect(subject).to redirect_to assigns(:codex)
       end
     end
 
     context 'when codices are not saved' do
-      subject{ post :create, codex: {name: nil} }
+      subject { post :create, codex: { name: nil } }
 
       it 'does not create codices' do
         expect{ subject }.to_not change{ Codex.count }
       end
 
-      it 'renders :new' do
-        expect(subject).to render_template(:new)
+      it 'assigns flash[:error]' do
+        subject
+
+        expect(flash[:error]).to be_present
       end
 
-      it 'adds a flash :error' do
+      it 'renders :new' do
+        expect(subject).to render_template :new
+      end
+    end
+  end
+
+  describe 'PUT update' do
+    let(:codex) { create :codex }
+
+    context 'when codices are saved' do
+      subject { put :update, id: codex, codex: { name: "#{codex.name}_2" } }
+
+      it 'updates a codex' do
+        expect{ subject }.to change{ codex.reload.name }
+      end
+
+      it 'assigns flash[:notice]' do
         subject
-        expect(flash[:error]).to be_present
+
+        expect(flash[:notice]).to be_present
+      end
+
+      it 're-directs to :show' do
+        expect(subject).to redirect_to codex
       end
     end
   end
