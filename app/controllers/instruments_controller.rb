47fb0123ef7bc14d740030cdd5353b97f1b860d4
Scott Macpherson <scottmacphersonmusic@gmail.com>
2017-09-19 14:26:32 -0700

    update instruments

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -9,6 +9,16 @@ class InstrumentsController < ApplicationController
     end
   end
 
+  def update
+    @instrument = Instrument.find(params[:id])
+    if @instrument.update_attributes(instrument_params)
+      redirect_to @instrument, notice: 'Instrument Saved'
+    else
+      flash.now[:error] = 'Instrument Not Saved'
+      render :edit
+    end
+  end
+
   private
 
   def instrument_params
--- spec/controllers/instruments_controller_spec.rb
+++ spec/controllers/instruments_controller_spec.rb
@@ -36,4 +36,42 @@ describe InstrumentsController do
       end
     end
   end
+
+  describe 'PUT update' do
+    let(:instrument) { create :instrument }
+
+    context 'when successful' do
+      subject { put :update, id: instrument, instrument: { name: 'new_name' } }
+
+      it 'updates an instrument' do
+        expect{ subject }.to change{ instrument.reload.name }
+      end
+
+      it 'assigns flash[:notice]' do
+        subject
+        expect(flash[:notice]).to be_present
+      end
+
+      it 'redirects to @instrument' do
+        expect(subject).to redirect_to assigns(:instrument)
+      end
+    end
+
+    context 'when unsuccessful' do
+      subject { put :update, id: instrument, instrument: { name: nil } }
+
+      it 'does not update an instrument' do
+        expect{ subject }.to_not change{ instrument.reload.name }
+      end
+
+      it 'assigns flash[:error]' do
+        subject
+        expect(flash[:error]).to be_present
+      end
+
+      it 'renders :edit' do
+        expect(subject).to render_template :edit
+      end
+    end
+  end
 end
